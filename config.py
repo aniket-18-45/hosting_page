39119c3239026bb06340c6fe65747aa43343e4e8
aniket <aniketdhiman162008@gmail.com>
2026-05-19 20:02:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-  SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     MYSQL_HOST = os.environ.get('MYSQL_HOST', 'mysql.railway.internal')
     MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'JUORraZQXgcNCpmbVddejcTwHFARIMkl')
