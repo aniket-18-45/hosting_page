5e726860e590e9e27cebb0b4de33522fd85bb3d7
aniket <aniketdhiman162008@gmail.com>
2026-05-19 21:21:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'mysql.railway.internal')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'JUORraZQXgcNCpmbVddejcTwHFARIMkl')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'voice_assistant_db')
+    MYSQL_HOST = os.environ.get('MYSQLHOST')
+    MYSQL_USER = os.environ.get('MYSQLUSER')
+    MYSQL_PASSWORD = os.environ.get('MYSQLPASSWORD')
+    MYSQL_DB = os.environ.get('MYSQLDATABASE')
+    MYSQL_PORT = int(os.environ.get('MYSQLPORT', 3306))
